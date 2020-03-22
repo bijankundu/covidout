e8d83d6bc4452a116fb24779dd03f0357620a7d7
Bijan Kundu <bijankundu025@gmail.com>
2020-03-22 20:17:14 +0530

    changed API server

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ let countries;
 let country = "India";
 let world_stats, country_stats, india_status;
 
+
 const app = express();
 
 app.set("view engine", "ejs");
@@ -132,54 +133,60 @@ app.get("/sources", (req, res) => {
 
 app.get("/country/:countryName", (req, res) => {
     country = _.lowerCase(req.params.countryName);
-    serve = unirest("GET", "https://coronavirus-monitor.p.rapidapi.com/coronavirus/latest_stat_by_country.php");
+    // serve = unirest("GET", "https://coronavirus-monitor.p.rapidapi.com/coronavirus/latest_stat_by_country.php");
 
-    serve.query({
-        "country": country
-    });
+    // serve.query({
+    //     "country": country
+    // });
 
-    serve.headers({
-        "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
-        "x-rapidapi-key": "f9a8a5b0eemsh7d728929b6747e7p161899jsn9bb7591ffe1c"
-    });
+    // serve.headers({
+    //     "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
+    //     "x-rapidapi-key": "f9a8a5b0eemsh7d728929b6747e7p161899jsn9bb7591ffe1c"
+    // });
 
 
-    serve.end(function (res) {
-        if (res.error) throw new Error(res.error);
-        country_stats = JSON.parse(res.body);
-        // console.log(country_stats);
-    });
+    // serve.end(function (res) {
+    //     if (res.error) throw new Error(res.error);
+    //     country_stats = JSON.parse(res.body);
+    //     // console.log(country_stats);
+    // });
+    fetch('https://corona.lmao.ninja/countries/' + country)
+        .then(res => res.json())
+        .then((json) => {
+            country_stats = json;
 
-    let today = new Date();
-    const options = {
-        year: 'numeric',
-        month: 'short',
-        day: 'numeric'
-    };
-    let country_total_cases = country_stats.latest_stat_by_country[0].total_cases;
 
-    if (country_stats.latest_stat_by_country[0].serious_critical == "")
-        country_stats.latest_stat_by_country[0].serious_critical = "0";
-    res.render('base', {
-        country: country,
-        today: today.toLocaleDateString('en-US', options),
-        world_total: world_stats.total_cases,
-        world_new: world_stats.new_cases,
-        world_recovered: world_stats.total_recovered,
-        world_new_death: world_stats.new_deaths,
-        world_dead: world_stats.total_deaths,
-        world_dead_percentage: (Math.floor((Number(world_stats.total_deaths.replace(",", "")) / Number(world_stats.total_cases.replace(",", ""))) * 100)),
-        country_total: country_total_cases,
-        country_total_percentage: (Math.ceil((Number(country_total_cases.replace(",", "")) / Number(world_stats.total_cases.replace(",", ""))) * 100)),
-        country_active: country_stats.latest_stat_by_country[0].active_cases,
-        country_active_cases_percentage: (Math.ceil((Number(country_stats.latest_stat_by_country[0].active_cases.replace(",", "")) / Number(country_total_cases.replace(",", ""))) * 100)),
-        country_recovered: country_stats.latest_stat_by_country[0].total_recovered,
-        country_recovered_percentage: (Math.ceil((Number(country_stats.latest_stat_by_country[0].total_recovered.replace(",", "")) / Number(country_total_cases.replace(",", ""))) * 100)),
-        country_critical: country_stats.latest_stat_by_country[0].serious_critical,
-        country_critical_percentage: (Math.ceil((Number(country_stats.latest_stat_by_country[0].serious_critical.replace(",", "")) / Number(country_total_cases.replace(",", ""))) * 100)),
-        country_dead: country_stats.latest_stat_by_country[0].total_deaths,
-        country_dead_percentage: (Math.ceil((Number(country_stats.latest_stat_by_country[0].total_deaths.replace(",", "")) / Number(country_total_cases.replace(",", ""))) * 100))
-    });
+            let today = new Date();
+            const options = {
+                year: 'numeric',
+                month: 'short',
+                day: 'numeric'
+            };
+            let country_total_cases = country_stats.cases;
+
+            // if (country_stats.latest_stat_by_country[0].serious_critical == "")
+            //     country_stats.latest_stat_by_country[0].serious_critical = "0";
+            res.render('base', {
+                country: country,
+                today: today.toLocaleDateString('en-US', options),
+                world_total: world_stats.total_cases,
+                world_new: world_stats.new_cases,
+                world_recovered: world_stats.total_recovered,
+                world_new_death: world_stats.new_deaths,
+                world_dead: world_stats.total_deaths,
+                world_dead_percentage: (Math.floor((Number(world_stats.total_deaths.replace(",", "")) / Number(world_stats.total_cases.replace(",", ""))) * 100)),
+                country_total: country_total_cases.toLocaleString().replace(" ", ","),
+                country_total_percentage: (Math.ceil((Number(country_total_cases) / Number(world_stats.total_cases.replace(",", ""))) * 100)),
+                country_active: country_stats.active.toLocaleString().replace(" ", ","),
+                country_active_cases_percentage: (Math.ceil((Number(country_stats.active) / Number(country_total_cases)) * 100)),
+                country_recovered: country_stats.recovered.toLocaleString().replace(" ", ","),
+                country_recovered_percentage: (Math.ceil((Number(country_stats.recovered) / Number(country_total_cases)) * 100)),
+                country_critical: country_stats.critical.toLocaleString().replace(" ", ","),
+                country_critical_percentage: (Math.ceil((Number(country_stats.critical) / Number(country_total_cases)) * 100)),
+                country_dead: country_stats.deaths.toLocaleString().replace(" ", ","),
+                country_dead_percentage: (Math.ceil((Number(country_stats.deaths) / Number(country_total_cases)) * 100))
+            });
+        });
 });
 
 
@@ -193,3 +200,5 @@ app.get("/affected-countries", (req, res) => {
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running on port 3000");
 });
+
+// https://corona.lmao.ninja/countries/india
