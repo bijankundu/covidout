587fb3be237a411f1b7db47eaea49314dea0d9fc
Bijan Kundu <bijankundu025@gmail.com>
2020-04-19 18:59:28 +0530

    updated number formatting in Indian format

--- app.js
+++ app.js
@@ -36,11 +36,18 @@ app.use(express.static("public"));
 //   // console.log(world_stats);
 // });
 
+function indianFormat(x) {
+  let lastThree = x.substring(x.length - 3);
+  let otherNumbers = x.substring(0, x.length - 3);
+  if (otherNumbers != "") lastThree = "," + lastThree;
+  let result = otherNumbers.replace(/\B(?=(\d{2})+(?!\d))/g, ",") + lastThree;
+  return result;
+}
 fetch("https://corona.lmao.ninja/v2/all?yesterday=false")
   .then((res) => res.json())
   .then((json) => {
     world_stats = json;
-    // console.log(world_stats);
+    //console.log(typeof(world_stats.cases.toString()));
 
     serve = unirest(
       "GET",
@@ -55,7 +62,7 @@ fetch("https://corona.lmao.ninja/v2/all?yesterday=false")
     serve.end(function (res) {
       if (res.error) throw new Error(res.error);
       countries = JSON.parse(res.body);
-      console.log(countries.affected_countries);
+      //console.log(countries.affected_countries);
     });
     app.get("/", (req, res) => {
       // serve = unirest("GET", "https://coronavirus-monitor.p.rapidapi.com/coronavirus/latest_stat_by_country.php");
@@ -93,49 +100,57 @@ fetch("https://corona.lmao.ninja/v2/all?yesterday=false")
           res.render("home", {
             country: "india",
             today: today.toLocaleDateString("en-US", options),
-            world_total: world_stats.cases.toLocaleString().replace(" ", ","),
-            world_active: world_stats.active.toLocaleString().replace(" ", ","),
+            world_total: world_stats.cases
+              .toString()
+              .replace(/(\d)(?=(\d\d)+\d$)/g, "$1,"),
+            world_active: world_stats.active
+              .toString()
+              .replace(/(\d)(?=(\d\d)+\d$)/g, "$1,"),
             world_recovered: world_stats.recovered
-              .toLocaleString()
-              .replace(" ", ","),
+              .toString()
+              .replace(/(\d)(?=(\d\d)+\d$)/g, "$1,"),
             world_affected: world_stats.affectedCountries
-              .toLocaleString()
-              .replace(" ", ","),
-            world_dead: world_stats.deaths.toLocaleString().replace(" ", ","),
+              .toString()
+              .replace(/(\d)(?=(\d\d)+\d$)/g, "$1,"),
+            world_dead: world_stats.deaths
+              .toString()
+              .replace(/(\d)(?=(\d\d)+\d$)/g, "$1,"),
             world_dead_percentage: Math.floor(
               (Number(world_stats.deaths) / Number(world_stats.cases)) * 100
             ),
             india_total: india_status_total_cases
-              .toLocaleString()
-              .replace(" ", ","),
+              .toString()
+              .replace(/(\d)(?=(\d\d)+\d$)/g, "$1,"),
             india_total_percentage: Math.ceil(
               (Number(india_status_total_cases) / Number(world_stats.cases)) *
                 100
             ),
             india_active: india_status.active
-              .toLocaleString()
-              .replace(" ", ","),
+              .toString()
+              .replace(/(\d)(?=(\d\d)+\d$)/g, "$1,"),
             india_active_cases_percentage: Math.ceil(
               (Number(india_status.active) / Number(india_status_total_cases)) *
                 100
             ),
             india_recovered: india_status.recovered
-              .toLocaleString()
-              .replace(" ", ","),
+              .toString()
+              .replace(/(\d)(?=(\d\d)+\d$)/g, "$1,"),
             india_recovered_percentage: Math.ceil(
               (Number(india_status.recovered) /
                 Number(india_status_total_cases)) *
                 100
             ),
             india_critical: india_status.critical
-              .toLocaleString()
-              .replace(" ", ","),
+              .toString()
+              .replace(/(\d)(?=(\d\d)+\d$)/g, "$1,"),
             india_critical_percentage: Math.ceil(
               (Number(india_status.critical) /
                 Number(india_status_total_cases)) *
                 100
             ),
-            india_dead: india_status.deaths.toLocaleString().replace(" ", ","),
+            india_dead: india_status.deaths
+              .toString()
+              .replace(/(\d)(?=(\d\d)+\d$)/g, "$1,"),
             india_dead_percentage: Math.ceil(
               (Number(india_status.deaths) / Number(india_status_total_cases)) *
                 100
@@ -202,47 +217,53 @@ fetch("https://corona.lmao.ninja/v2/all?yesterday=false")
           res.render("base", {
             country: country,
             today: today.toLocaleDateString("en-US", options),
-            world_total: world_stats.cases.toLocaleString().replace(" ", ","),
-            world_active: world_stats.active.toLocaleString().replace(" ", ","),
+            world_total: world_stats.cases
+              .toString()
+              .replace(/(\d)(?=(\d\d)+\d$)/g, "$1,"),
+            world_active: world_stats.active
+              .toString()
+              .replace(/(\d)(?=(\d\d)+\d$)/g, "$1,"),
             world_recovered: world_stats.recovered
-              .toLocaleString()
-              .replace(" ", ","),
+              .toString()
+              .replace(/(\d)(?=(\d\d)+\d$)/g, "$1,"),
             world_affected: world_stats.affectedCountries
-              .toLocaleString()
-              .replace(" ", ","),
-            world_dead: world_stats.deaths.toLocaleString().replace(" ", ","),
+              .toString()
+              .replace(/(\d)(?=(\d\d)+\d$)/g, "$1,"),
+            world_dead: world_stats.deaths
+              .toString()
+              .replace(/(\d)(?=(\d\d)+\d$)/g, "$1,"),
             world_dead_percentage: Math.floor(
               (Number(world_stats.deaths) / Number(world_stats.cases)) * 100
             ),
             country_total: country_total_cases
-              .toLocaleString()
-              .replace(" ", ","),
+              .toString()
+              .replace(/(\d)(?=(\d\d)+\d$)/g, "$1,"),
             country_total_percentage: Math.ceil(
               (Number(country_total_cases) / Number(world_stats.cases)) * 100
             ),
             country_active: country_stats.active
-              .toLocaleString()
-              .replace(" ", ","),
+              .toString()
+              .replace(/(\d)(?=(\d\d)+\d$)/g, "$1,"),
             country_active_cases_percentage: Math.ceil(
               (Number(country_stats.active) / Number(country_total_cases)) * 100
             ),
             country_recovered: country_stats.recovered
-              .toLocaleString()
-              .replace(" ", ","),
+              .toString()
+              .replace(/(\d)(?=(\d\d)+\d$)/g, "$1,"),
             country_recovered_percentage: Math.ceil(
               (Number(country_stats.recovered) / Number(country_total_cases)) *
                 100
             ),
             country_critical: country_stats.critical
-              .toLocaleString()
-              .replace(" ", ","),
+              .toString()
+              .replace(/(\d)(?=(\d\d)+\d$)/g, "$1,"),
             country_critical_percentage: Math.ceil(
               (Number(country_stats.critical) / Number(country_total_cases)) *
                 100
             ),
             country_dead: country_stats.deaths
-              .toLocaleString()
-              .replace(" ", ","),
+              .toString()
+              .replace(/(\d)(?=(\d\d)+\d$)/g, "$1,"),
             country_dead_percentage: Math.ceil(
               (Number(country_stats.deaths) / Number(country_total_cases)) * 100
             ),
